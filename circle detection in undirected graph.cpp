8ace71de17824e67671ee7e167f89e26089984cc
Akash Mandal <50658274+Skycoder10@users.noreply.github.com>
2021-06-28 20:14:26 +0530

    Update circle detection in undirected graph.cpp

--- circle detection in undirected graph.cpp
+++ circle detection in undirected graph.cpp
@@ -1,1 +1,37 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+const int N=100005;
+
+
+std::vector<int>topo[1000];
+std::vector<int> ;
+
+
+
+void toposort(){
+    int node,edge;
+    cin>>node>>edge;
+    for (int i = 0; i < edge; i++) {
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+
+}
+ 
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int T=1;
+    //cin >>T;
+    while(T--){
+        solve();
+    }
+ 
+  return 0;
+}
